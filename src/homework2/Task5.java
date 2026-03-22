3b620cfc718822ab89433ceb09c7a53ee64bed82
Svetlana Pislyakova <pislakovasvetlana@gmail.com>
2026-03-22 07:22:49 +0300

    Homework2 is done

---
+++ src/homework2/Task5.java
@@ -0,0 +1,29 @@
+package homework2;
+
+import java.util.Scanner;
+
+/*
+Напишите программу, где пользователь вводит любое целое положительное
+число.
+А программа суммирует все числа от 1 до введенного пользователем числа. Для
+ввода числа воспользуйтесь классом Scanner. Сделать проверку, чтобы
+пользователь не мог ввести некорректные данные
+ */
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое положительное число:");
+        int a;
+        int result = 0;
+        do {
+            a = scanner.nextInt();
+            if (a <= 0) System.out.println("Число должно быть больше нуля, попробуйте еще раз:");
+        } while (a <= 0);
+        for (int i = 1; i <= a; i++) {
+            result += i;
+        }
+        System.out.println("Сумма чисел от 1 до " + a + " равна " + result);
+    }
+}
